fb4b27ffba4cae81e276ec6bbe818521876bb3c9
Yaroslav Petrukhin <75678049+YarPetru@users.noreply.github.com>
2022-07-19 21:17:32 +0300

    broken query

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,13 +1,13 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const contactsApi = createApi({
-  reduserPath: 'contactsApi',
+  reducerPath: 'contactsApi',
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://62d669ed15ad24cbf2d6b26d.mockapi.io/api/v1/',
   }),
   endpoints: builder => ({
     getContacts: builder.query({
-      query: () => `contacts`,
+      query: () => '/contacts',
     }),
   }),
 });
